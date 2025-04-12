9a306668e1009e4e7d98f54b850fa7e07e81f884
makeitsimpleD <denis@makeit-simple.de>
2025-04-13 01:55:26 +0200

    Clean commit

--- app.py
+++ app.py
@@ -27,7 +27,5 @@ def convert():
     except Exception as e:
         return {'error': str(e)}, 500
 
-if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
